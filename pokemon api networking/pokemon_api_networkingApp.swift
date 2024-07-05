0da74199aead2e57efd67cb73a9dee8d75c5c492
Hatice Nur <70744158+Haticenurcoskunn@users.noreply.github.com>
2024-07-05 18:23:18 +0300

    Initial Commit

---
+++ pokemon api networking/pokemon_api_networkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemon_api_networkingApp.swift
+//  pokemon api networking
+//
+//  Created by nur on 5.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct pokemon_api_networkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
